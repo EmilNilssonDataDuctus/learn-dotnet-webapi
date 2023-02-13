0241a6a45f0e22e578e1b9bc84938a581619ec78
Emil Nilsson <emil.nilsson@dataductus.se>
2023-02-13 13:01:22 +0100

    API works

--- MyFirstAPI/Controllers/ProductsController.cs
+++ MyFirstAPI/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Models;
 
 namespace MyFirstAPI.Controllers
 {
@@ -7,10 +8,17 @@ namespace MyFirstAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext context)
         {
-            return "OK";
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToArray();
         }
     }
 }
--- MyFirstAPI/Models/Product.cs
+++ MyFirstAPI/Models/Product.cs
@@ -6,8 +6,8 @@ namespace MyFirstAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Sku { get; set; }
-        public string Description { get; set; }
+        public string Sku { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
         public int CategoryId { get; set; }
