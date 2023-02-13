5d6d5033bfecd1aa5abcd5cf4e70d78c272562bf
Emil Nilsson <emil.nilsson@dataductus.se>
2023-02-13 14:49:25 +0100

    Add POST route

--- MyFirstAPI/Models/Product.cs
+++ MyFirstAPI/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MyFirstAPI.Models
 {
@@ -6,14 +7,16 @@ namespace MyFirstAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Sku { get; set; } = string.Empty;
+        public string Sku { get; set; }
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
+
+        [Required]
         public int CategoryId { get; set; }
 
         [JsonIgnore]
-        public virtual Category Category { get; set; }
+        public virtual Category? Category { get; set; }
 
     }
 }
